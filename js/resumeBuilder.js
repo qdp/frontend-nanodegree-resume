54d9452976e9b0ec902a645c3f78775ca583f88f
qdp <porterquaid@gmail.com>
2015-02-28 23:34:31 -0800

    Class 0 and 1 finished

---
+++ js/resumeBuilder.js
@@ -0,0 +1,72 @@
+
+var work = {
+    "jobs": [
+        {
+            "employer" : "PSI",
+            "title" : "Farm hand",
+            "location" : "Sacramento",
+            "dates_worked" : ['1/1/2001', "1/1/2004"],
+            "description" : "I farmed almonds"
+        },
+        {
+            "employer" : "Reddit",
+            "title" : "upvoter",
+            "location" : "Internet",
+            "dates_worked" : ["9/1/2012", "present"],
+            "description" : "I needed to upvote everything"
+        }
+    ]
+}
+
+var projects = {
+    "projects": [
+        {
+            "title": "run fast",
+            "dates_worked": ["1/1/2015", "present"],
+            "description": "Run forrest run",
+            "images": ["url string for project images?"]
+        },
+        {
+            "title": "Eat a chicken",
+            "dates_worked": ["1/1/2000", "1/2/2000"],
+            "description": "___raw___",
+            "images": []
+        },
+    ]
+}
+
+
+var bio = {
+    "name" : "Quaid Porter",
+    "contacts" : {
+        "email" : "quaidporter@gmail.com",
+        "phone" : "000-123-4567"
+    },
+    "picture_url" : "images/fry.jpg",
+    "role" : "Backend Python/Go programmer",
+    "welcome_message" : "welcome to my resume site!",
+    "skills" : ["Python", "Golang", "JS", "AngularJS"],
+}
+
+var education = {
+    "schools": [
+        {
+            "name" : "flying circuis",
+            "location" : "England",
+            "majors" : ["chemistry", "cs"],
+            "degree" : "git"
+        },
+        {
+            "name" : "hard kknocks",
+            "location" : "8 mile",
+            "majors" : ["chemistry"],
+            "degree" : "Eminem"
+        }
+    ],
+    "online_courses": [
+    {
+        "name": "Front end nano-degree",
+        "school": "Udacity.com"
+    }
+    ]
+}
